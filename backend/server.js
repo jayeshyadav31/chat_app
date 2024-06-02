64f3999df9f8218add0107b2036964b870ea7c8e
jayeshyadav31 <104785686+jayeshyadav31@users.noreply.github.com>
2024-06-02 22:50:06 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 import authRoute from './Routes/AuthRoutes.js'
 import connectDB from './db/connectDB.js';
 import messageRoute from './Routes/MessageRoutes.js'
@@ -7,18 +8,22 @@ import userRoutes from './Routes/UserRoutes.js'
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
 import { app,server,io } from './socket/socket.js';
+const __dirname=path.resolve()
 dotenv.config();
 app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
+
 const PORT=process.env.PORT || 3000
-connectDB()
-app.get('/',(req,res)=>{
-    res.send('i am at earth')
-})
 app.use("/api/auth",authRoute)
 app.use("/api/messages",messageRoute)
 app.use("/api/users/",userRoutes)
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+connectDB()
 server.listen(PORT,()=>{
     console.log(`server is listening to the port ${PORT}`);
 })
